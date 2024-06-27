2969623cca4504a0a28cfc8803d8b7ee8667069b
Анна <nkieru13@gmail.com>
2024-06-27 20:13:08 +0300

    intermediate

---
+++ news_portal/news_portal_pj/news_portal_app/tasks.py
@@ -0,0 +1,63 @@
+from datetime import datetime, timedelta
+# import datetime
+from .models import Post, Category
+from django.conf import settings
+from celery import shared_task
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
+
+@shared_task
+def weekly_news():
+    today = datetime.now()
+    last_week = today - timedelta(days=8)
+    posts = Post.objects.filter(date_time__gte=last_week)
+    categories = set(posts.values_list('categories__name', flat=True))
+    subscribers = set(Category.objects.filter(name__in=categories).values_list('subscribers__email', flat=True))
+
+    html_content = render_to_string(
+        'weekly_news.html',
+        {
+            'link': settings.SITE_URL,
+            'posts': posts,
+        }
+        )
+
+    msg = EmailMultiAlternatives(
+        subject='Новости за неделю',
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=subscribers,
+    )
+
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
+
+
+@shared_task
+def new_post(pk):
+    post = Post.objects.get(pk=pk)
+    categories = post.categories.all()
+    header = post.header
+    subscribers_emails = []
+
+    for c in categories:
+        subscribers = c.subscribers.all()
+        subscribers_emails += [i.email for i in subscribers]
+
+    html_content = render_to_string(
+        'subscribers_new_mail.html',
+        {'text': post.header,
+         'link': f' {settings.SITE_URL}/news/{pk}'
+         }
+
+    )
+
+    msg = EmailMultiAlternatives(
+        subject=header,
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=subscribers_emails,
+    )
+
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
---
+++ news_portal/news_portal_pj/news_portal_pj/celery.py
@@ -0,0 +1,19 @@
+import os
+from celery import Celery
+from celery.schedules import crontab
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news_portal_pj.settings')
+
+app = Celery('news_portal_pj')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+# hour=8, minute=0, day_of_week='monday'
+app.conf.beat_schedule = {
+    'action_every_monday_8am': {
+        'task': 'news_portal_app.tasks.weekly_news',
+        'schedule': crontab(),
+        'args': ()
+    },
+}
+
